3b64edc02290c2db7c9d7610bd5f761099034419
zachvenables <54538516+zachvenables@users.noreply.github.com>
2023-07-03 09:29:01 -0400

    changed cmd window, reduced data, tested

--- main.py
+++ main.py
@@ -3,15 +3,19 @@ from dotenv import load_dotenv
 from pprint import pprint
 import requests
 
+os.system('mode con: lines=60')
+
 load_dotenv()
 
 URL = "https://api.weatherapi.com/v1/forecast.json"
-PARAMS = {'q': '43206', 'days': 3, 'key': os.getenv('API_KEY')}
+PARAMS = {'q': '43206', 'days': 2, 'key': os.getenv('API_KEY')}
 
 r = requests.get(url = URL, params = PARAMS)
 
 data = r.json()
 days = data['forecast']['forecastday']
+
+#TODO make some indicator for current hour
 for day in days:
     print(day['date'])
     hours = day['hour']
@@ -23,3 +27,5 @@ for day in days:
             hour['condition']['text']
         ]
         pprint(weather)
+
+input("Finished. Press Ctrl + C to end.")
